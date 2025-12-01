84075525a298389c81a98daa82cfc383af223993
Max Dukov <dukov@arrival.com>
2025-12-01 20:59:02 +0300

    first commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,6 +1,10 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -11,10 +15,33 @@ const (
 	mInKm = 1000
 )
 
+// parsePackage разбирает строку данных о количестве шагов и продолжительности прогулки
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	vals := strings.Split(data, " ")
+	if len(vals) != 2 {
+		return 0, 0, errors.New("Неверный формат данных")
+	}
+	steps, err := strconv.Atoi(vals[0])
+	if err != nil {
+		return 0, 0, errors.New("Неверный формат данных")
+	}
+	duration, err := time.ParseDuration(vals[1])
+	if err != nil {
+		return 0, 0, errors.New("Неверный формат данных")
+	}
+	return steps, duration, nil
 }
 
+// DayActionInfo возвращает информацию о количестве шагов, дистанции и затраченных калориях
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	steps, _, err := parsePackage(data)
+	if err != nil {
+		return ""
+	}
+	if steps <= 0 {
+		return ""
+	}
+	distance := float64(steps) * stepLength / mInKm
+	calories := float64(steps) * weight * 0.5 / mInKm
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, distance, calories)
 }
